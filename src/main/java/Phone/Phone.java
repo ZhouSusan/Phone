fd61d9e7131c8b43f789d05dc28e0b547048775f
ZhouSusan <szhou089@gmail.com>
2022-11-25 21:34:32 -0800

    upated Phone class with attributes and constructor in Phone.java

--- src/main/java/Phone/Phone.java
+++ src/main/java/Phone/Phone.java
@@ -1,7 +1,16 @@
 package Phone;
 
-public class Phone {
-    public static void main(String[] args) {
-        System.out.println("hello World");
+public abstract class Phone {
+
+    private String versionNumber;
+    private int batteryPercentage;
+    private String ringTone;
+    private String phoneCarrier;
+
+    public Phone(String versionNumber, int batteryPercentage, String ringTone, String phoneCarrier) {
+        this.versionNumber = versionNumber;
+        this.batteryPercentage = batteryPercentage;
+        this.ringTone = ringTone;
+        this.phoneCarrier = phoneCarrier;
     }
 }
